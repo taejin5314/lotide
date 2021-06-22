a344c8134eba85dfa54827e60b383cac8124c61d
Taejin Jung <taejin5314@gmail.com>
2021-06-22 20:56:09 +0000

    Function to find the key by the address

---
+++ findKeyByValue.js
@@ -0,0 +1,21 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function (shows, value) {
+  let result;
+  for (const genre in shows) {
+    if (value === shows[genre]) result = genre;
+  }
+  return result;
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
