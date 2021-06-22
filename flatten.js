4823a86c6f139187d45a1088a3e2eb4fe1cb6f65
Taejin Jung <taejin5314@gmail.com>
2021-06-22 18:46:24 +0000

    fixed flatten function

--- flatten.js
+++ flatten.js
@@ -15,6 +15,16 @@ const assertArraysEqual = function (actual, expected) {
   else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-const flatten = function (array) {
-
+const flatten = function (arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        newArr.push(arr[i][j]);
+      }
+    } else newArr.push(arr[i]);
+  }
+  return newArr;
 }
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]))
