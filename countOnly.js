f3ee862a272129de18c2e465535758b42e213d50
Taejin Jung <taejin5314@gmail.com>
2021-06-23 20:02:21 +0000

    Take until function

--- countOnly.js
+++ countOnly.js
@@ -1,11 +1,11 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
-const countOnly = function(allItems, itemsToCount) {
+const countOnly = function (allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
     if (itemsToCount[item]) {
@@ -38,6 +38,7 @@ const result1 = countOnly(firstNames, {
   "Agouhanna": false
 });
 
+console.log(result1);
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
---
+++ takeUntil.js
@@ -0,0 +1,37 @@
+const takeUntil = function (array, callback) {
+  const result = [];
+  let i = 0;
+  while (!callback(array[i])) {
+    result.push(array[i])
+    i++;
+  }
+  return result;
+}
+
+const eqArrays = (actual, expected) => {
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) return false;
+    }
+    return true;
+  }
+  return false;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  const inspect = require('util').inspect
+  if (eqArrays(actual, expected)) console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  else console.log(`🔴🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2])
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"])
