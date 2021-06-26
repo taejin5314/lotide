3304970aeda66988a3578e5c54d7b511eafcf1a1
Taejin Jung <taejin5314@gmail.com>
2021-06-26 18:43:03 +0000

    update

--- eqArrays.js
+++ eqArrays.js
@@ -15,8 +15,8 @@ const eqArrays = (actual, expected) => {
   return false;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+// eqArrays([1, 2, 3], [1, 2, 3]); // => true
+// eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
