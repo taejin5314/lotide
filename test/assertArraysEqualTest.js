a2a777bc58a1a3176d027bce0abb520f4f085c57
Taejin Jung <taejin5314@gmail.com>
2021-06-28 20:57:16 +0000

    Comment out assertArraysEqual and assertEqual test cases

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,6 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual('Lighthouse Labs', 'Bootcamp');
-assertArraysEqual(1, 1);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 5]);
+// assertArraysEqual('Lighthouse Labs', 'Bootcamp');
+// assertArraysEqual(1, 1);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [1, 2, 5]);
