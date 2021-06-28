73d17b0f5d93cc158207de5964ffc81c5a57bd0b
Taejin Jung <taejin5314@gmail.com>
2021-06-28 19:48:39 +0000

    Refactoring flatten.js and add test cases

--- flatten.js
+++ flatten.js
@@ -1,28 +1,9 @@
-// const eqArrays = (actual, expected) => {
-//   if (actual.length === expected.length) {
-//     for (let i = 0; i < actual.length; i++) {
-//       if (actual[i] !== expected[i]) return false;
-//     }
-//     return true;
-//   }
-//   return false;
-// };
-
-// const assertArraysEqual = function(actual, expected) {
-//   if (eqArrays(actual, expected)) console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-// };
-
 const flatten = function(arr) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])) {
-      for (let j = 0; j < arr[i].length; j++) {
-        newArr.push(arr[i][j]);
-      }
-    } else newArr.push(arr[i]);
+  if (arr.length === 0) return arr;
+  if (Array.isArray(arr[0])) {
+    return flatten(arr[0]).concat(flatten(arr.slice(1)));
   }
-  return newArr;
+  return arr.slice(0, 1).concat(flatten(arr.slice(1)));
 };
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
+module.exports = flatten;
