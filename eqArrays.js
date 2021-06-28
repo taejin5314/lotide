0cdb1299e181fc8a8a4d3d50903f466c0cba6e96
Taejin Jung <taejin5314@gmail.com>
2021-06-28 16:29:33 +0000

    Refactor eqArrays(testing mode)

--- eqArrays.js
+++ eqArrays.js
@@ -15,32 +15,4 @@ const eqArrays = (actual, expected) => {
   return false;
 };
 
-// eqArrays([1, 2, 3], [1, 2, 3]); // => true
-// eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-assertEqual(eqArrays([
-  [2, 3],
-  [4]
-], [
-  [2, 3],
-  [4]
-]), true); // => true
-
-assertEqual(eqArrays([
-  [2, 3],
-  [4]
-], [
-  [2, 3],
-  [4, 5]
-]), false); // => false
-assertEqual(eqArrays([
-  [2, 3],
-  [4]
-], [
-  [2, 3], 4
-]), false); // => false
+module.exports = eqArrays;
