35c9b6e1271bad462fb344b54d8cefc61ab4589a
Taejin Jung <taejin5314@gmail.com>
2021-06-28 16:32:29 +0000

    Edit eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const eqArrays = (actual, expected) => {
   if (actual.length === expected.length) {
     for (let i = 0; i < actual.length; i++) {
