8cc987ff98bdc8ccd2e1777a07b6993ca4bde0d7
Taejin Jung <taejin5314@gmail.com>
2021-06-28 17:34:19 +0000

    Use assert.deepEqual instead of expect().to.eql()

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,28 +1,28 @@
-const expect = require('chai').expect;
+const assert = require('chai').assert;
 const middle = require('../middle');
 
 describe('#middle', () => {
   it("returns [] for [1]", () => {
-    expect(middle([1])).to.eql([]);
+    assert.deepEqual(middle([1]), []);
   });
 
   it("returns [] for [1, 2]", () => {
-    expect(middle([1, 2])).to.eql([]);
+    assert.deepEqual(middle([1, 2]), []);
   });
 
   it("returns [2] for [1, 2, 3]", () => {
-    expect(middle([1, 2, 3])).to.eql([2]);
+    assert.deepEqual(middle([1, 2, 3]), [2]);
   });
 
   it("returns [2, 3] for [1, 2, 3, 4]", () => {
-    expect(middle([1, 2, 3, 4])).to.eql([2, 3]);
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
 
   it("returns [3] for [1, 2, 3, 4, 5]", () => {
-    expect(middle([1, 2, 3, 4, 5])).to.eql([3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
 
   it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
-    expect(middle([1, 2, 3, 4, 5, 6])).to.eql([3, 4]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,8 @@
-const expect = require('chai').expect;
+const assert = require('chai').assert;
 const tail = require('../tail');
 
 describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
-    expect(tail(['Hello', 'Lighthouse', 'Labs'])).to.eql(['Lighthouse', 'Labs']);
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
   });
 });
